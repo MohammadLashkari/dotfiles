7894ef70b6fe8a9ac4e61feb1d404f6d07a86184
Mohammad Lashkari <moe.lashkari@gmail.com>
2024-09-21 11:35:33 +0330

    Update keymap.lua

--- .config/nvim/lua/keymap.lua
+++ .config/nvim/lua/keymap.lua
@@ -8,11 +8,12 @@ vim.keymap.set('n', '<leader>Y', [["+Y]])
 
 vim.keymap.set('v', 'J', ":m '>+1<cr>gv=gv")
 vim.keymap.set('v', 'K', ":m '<-2<cr>gv=gv")
+
 vim.keymap.set("n", "<c-n>", "<cmd>cnext<cr>zz")
 vim.keymap.set("n", "<c-p>", "<cmd>cprev<cr>zz")
 
 vim.keymap.set('n', '<c-d>', '<c-d>zz')
-vim.keymap.set('n', '<c-d>', '<c-d>zz')
+vim.keymap.set('n', '<c-u>', '<c-u>zz')
 
 vim.keymap.set('n', '<leader>s', [[:%s/<c-r><c-w>//gI<left><left><left>]])
 vim.keymap.set('v', '<leader>s', [[o:s/<c-r><c-w>//gI<left><left><left>]])
@@ -21,4 +22,5 @@ vim.keymap.set('n', '<c-right>', '<c-w>5>')
 vim.keymap.set('n', '<c-left>', '<c-w>5<')
 vim.keymap.set('n', '<c-up>', '<c-w>5+')
 vim.keymap.set('n', '<c-down>', '<c-w>5-')
+
 vim.keymap.set('n', '<leader>vd', '<cmd>vsplit | lua vim.lsp.buf.definition()<cr>')
