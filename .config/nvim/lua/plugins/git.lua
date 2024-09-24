c91ab42751e2d9e96da86f9d458fac480125b700
Mohammad Lashkari <moe.lashkari@gmail.com>
2024-09-24 19:26:32 +0330

    Update git.lua

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -2,11 +2,6 @@ return {
     {
         "tpope/vim-fugitive",
         event = "VeryLazy",
-        config = function()
-            vim.keymap.set("n", "<leader>G", vim.cmd.Git)
-            vim.keymap.set("n", "<leader>gh", ":diffget //2<cr>")
-            vim.keymap.set("n", "<leader>gl", ":diffget //3<cr>")
-        end,
     },
     {
         'lewis6991/gitsigns.nvim',
