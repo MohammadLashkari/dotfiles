dd4f372250e5a8770453800da5afd9760d1c2edc
Mohammad Lashkari <moe.lashkari@gmail.com>
2025-04-02 20:54:10 +0330

    Update git.lua

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -7,7 +7,34 @@ return {
         'lewis6991/gitsigns.nvim',
         event = "VeryLazy",
         config = function()
-            require('gitsigns').setup()
+            require('gitsigns').setup({
+                on_attach = function(bufnr)
+                    local gitsigns = require('gitsigns')
+                    local function map(mode, l, r, opts)
+                        opts = opts or {}
+                        opts.buffer = bufnr
+                        vim.keymap.set(mode, l, r, opts)
+                    end
+
+                    -- Navigation
+                    map('n', ']c', function()
+                        if vim.wo.diff then
+                            vim.cmd.normal({ ']c', bang = true })
+                        else
+                            gitsigns.nav_hunk('next')
+                        end
+                    end)
+                    map('n', '[c', function()
+                        if vim.wo.diff then
+                            vim.cmd.normal({ '[c', bang = true })
+                        else
+                            gitsigns.nav_hunk('prev')
+                        end
+                    end)
+                    -- Actions
+                    map('n', '<leader>hp', gitsigns.preview_hunk)
+                end
+            })
         end,
     },
 }
