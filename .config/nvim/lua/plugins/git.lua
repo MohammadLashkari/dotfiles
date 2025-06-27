c99be857ed58b4a664490e9424c0ff279c83ec4a
Mohammad Lashkari <moe.lashkari@gmail.com>
2025-06-27 11:23:20 +0330

    Update git.lua

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -1,5 +1,16 @@
 return {
 	{
+		"mbbill/undotree",
+		event = "VeryLazy",
+		config = function()
+			vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
+			vim.g.undotree_WindowLayout = 2
+			vim.g.undotree_SetFocusWhenToggle = true
+			vim.g.undotree_DiffAutoOpen = false
+			vim.g.undotree_HelpLine = false
+		end,
+	},
+	{
 		"tpope/vim-fugitive",
 		event = "VeryLazy",
 	},
